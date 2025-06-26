d6e779e532e951933f8eb95522033fa08db92cf5
Lorenzo Machado <lorenzolima1809@gmail.com>
2025-06-26 09:09:32 -0300

    fix(arango): conecta com usuário root

--- src/database/arango.ts
+++ src/database/arango.ts
@@ -5,4 +5,8 @@ dotenv.config();
 export const db = new Database({
   url: process.env.ARANGO_URL,
   databaseName: "_system",
+  auth: {                                      
+    username: process.env.ARANGO_USER || "root",
+    password: process.env.ARANGO_PASSWORD || "root",
+  },
 });
